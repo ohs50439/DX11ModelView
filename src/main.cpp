d7bd803b69c7f55d9c896d449ecb79858df7b21e
ohs50439@gmail.com <ohs50439@gmail.com>
2016-07-26 23:58:54 +0900

    [Update] Shader周りの初期化にコメント追加

--- src/main.cpp
+++ src/main.cpp
@@ -36,20 +36,20 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	};
 
 	// VetexBufferの格納先を宣言
-	ID3D11Buffer *vertexbuffer = nullptr;
+	ID3D11Buffer *vertexbuffer = nullptr; 
 
-	D3D11_BUFFER_DESC bd; // 生成方法
-	ZeroMemory(&bd, sizeof(bd)); // 中身をクリア
+	D3D11_BUFFER_DESC bd; // 生成方法(バッファー リソース)
+	ZeroMemory(&bd, sizeof(bd)); // 中身をゼロクリア
 	// Bufferの生成方法の格納
-	bd.Usage = D3D11_USAGE_DYNAMIC;
-	bd.ByteWidth = sizeof(vertex);
+	bd.Usage = D3D11_USAGE_DYNAMIC; // バッファーで想定されている読み込みおよび書き込みの方法を識別
+	bd.ByteWidth = sizeof(vertex);  // バッファーのサイズ(バイト単位)
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER; // なんのバッファですか？
 	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; // CPUからは書き込みのみ行います
 	// Bufferの生成
 	device.getDevice()->CreateBuffer(&bd, nullptr, &vertexbuffer);
 
 	//　頂点情報を格納していく
-	D3D11_MAPPED_SUBRESOURCE ms; // Bufferを格納する為にとりあえずロックをかけないといけない。どこまでロックをかける？
+	D3D11_MAPPED_SUBRESOURCE ms; // Bufferを格納する為にとりあえずロックをかけないといけない。どこまでロックをかける？サブリソース データにアクセスできるようにする
 	device.getContext()->Map(vertexbuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms); // アクセス先ms
 	memcpy(ms.pData, vertex, sizeof(vertex));// pData = vetexコピー　書き込み
 	device.getContext()->Unmap(vertexbuffer, NULL); // ロック解除
@@ -65,45 +65,44 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 
 
 	// 
-	ID3D10Blob *vsblob, *psblob;
+	ID3D10Blob *vsblob, *psblob; // 任意長のデータを返す際に使用
+	// ファイルを元にエフェクトをコンパイル
 	D3DX11CompileFromFile(TEXT("./Shader/VS.hlsl"), 0, 0, "main", "vs_5_0", 0, 0, 0, &vsblob, 0, 0);
 	D3DX11CompileFromFile(TEXT("./Shader/PS.hlsl"), 0, 0, "main", "ps_5_0", 0, 0, 0, &psblob, 0, 0);
 	// blobを_bufに格納
-	device.getDevice()->CreateVertexShader(vsblob->GetBufferPointer(), vsblob->GetBufferSize(), nullptr ,&vs_buf);
-	device.getDevice()->CreatePixelShader(psblob->GetBufferPointer(), psblob->GetBufferSize(), nullptr, &ps_buf);
+	device.getDevice()->CreateVertexShader(vsblob->GetBufferPointer(), vsblob->GetBufferSize(), nullptr ,&vs_buf); // コンパイル済みシェーダーから、頂点シェーダー オブジェクトを作成
+	device.getDevice()->CreatePixelShader(psblob->GetBufferPointer(), psblob->GetBufferSize(), nullptr, &ps_buf);  // ピクセル シェーダーを作成
 	// Shagerの設定
-	device.getContext()->VSSetShader(vs_buf, nullptr, 0);
-	device.getContext()->PSSetShader(ps_buf, nullptr, 0);
+	device.getContext()->VSSetShader(vs_buf, nullptr, 0); // 頂点シェーダーをデバイスに設定
+	device.getContext()->PSSetShader(ps_buf, nullptr, 0); // ピクセル シェーダーをデバイスに設定
 
 	//　インプットレイアウトを使うために必要なもの 
-	D3D11_INPUT_ELEMENT_DESC element[] = {
+	D3D11_INPUT_ELEMENT_DESC element[] = { // 入力アセンブラー ステージの単一の要素( HLSL セマンティクス,要素のセマンティクス インデックス,要素データのデータ型,入力アセンブラーを識別する整数値,各要素間のオフセット (バイト単位),単一の入力スロットの入力データ クラスを識別,インスタンス単位の同じデータを使用して描画するインスタンスの数)
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },// 位置情報
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0 } // UV情報 
 	};
-	ID3D11InputLayout *inputlayout;
-	device.getDevice()->CreateInputLayout(element, ARRAYSIZE(element), vsblob->GetBufferPointer(), vsblob->GetBufferSize(), &inputlayout); // 格納
+	ID3D11InputLayout *inputlayout; // 入力アセンブラー ステージの入力データにアクセス
+	device.getDevice()->CreateInputLayout(element, ARRAYSIZE(element), vsblob->GetBufferPointer(), vsblob->GetBufferSize(), &inputlayout); // 格納(入力アセンブラー ステージで使用される入力バッファー データ)
 	//インプットレイアウトの設定
-	device.getContext()->IASetInputLayout(inputlayout);
+	device.getContext()->IASetInputLayout(inputlayout); // 入力アセンブラー ステージに入力レイアウト オブジェクトをバインド
 	// ラスライザの生成
-	ID3D11RasterizerState* rasterizer = nullptr;
+	ID3D11RasterizerState* rasterizer = nullptr; // ラスタライザー ステートにアクセス
 	D3D11_RASTERIZER_DESC rasterizerDesc = {
-		D3D11_FILL_SOLID, // ワイヤーフレーム (見えるよフレーム)
-		D3D11_CULL_FRONT, // 裏面ポリゴンをカリング
-		FALSE,
-		0,
-		0.0f,
-		FALSE,
-		FALSE,
-		FALSE,
-		FALSE,
-		FALSE
+		D3D11_FILL_SOLID, // ワイヤーフレーム (レンダリング時に使用する描画モードを決定)
+		D3D11_CULL_FRONT, // 裏面ポリゴンをカリング(指定の方向を向いている三角形が描画されないことを示す)
+		FALSE,			  // 三角形が前向きか後ろ向きかを決定する
+		0,				  // 指定のピクセルに加算する深度値
+		0.0f,             // ピクセルの最大深度バイアス
+		FALSE,			  // 指定のピクセルのスロープに対するスカラ
+		FALSE,			  // 距離に基づいてクリッピングを有効
+		FALSE,            // シザー矩形カリングを有効
+		FALSE,			  // マルチサンプリングのアンチエイリアシングを有効
+		FALSE			  //　線のアンチエイリアシングを有効
 	};
-	device.getDevice()->CreateRasterizerState(&rasterizerDesc, &rasterizer); // ラスタライザーの生成
-	// ラスタライザー設定
-	device.getContext()->RSSetState(rasterizer);
+	device.getDevice()->CreateRasterizerState(&rasterizerDesc, &rasterizer); // ラスタライザーの生成（ラスタライザー ステージに動作を指示するラスタライザー ステート オブジェクト）
+	device.getContext()->RSSetState(rasterizer); // ラスタライザー設定(パイプラインのラスタライザー ステージのラスタライザー ステートを設定)
 
-	// 頂点の並び方の設定
-	device.getContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	device.getContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP); // 頂点の並び方の設定(プリミティブ タイプおよびデータの順序に関する情報をバインド)
 
 	// Shaderに送る行列の生成
 	ConstantBuffer mtx;
@@ -127,9 +126,9 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	bd.StructureByteStride = sizeof(float);
 	device.getDevice()->CreateBuffer(&bd, NULL, &constantbuffer); // バッファの生成
 	// Bufferに更新をかける 
-	device.getContext()->UpdateSubresource(constantbuffer, 0, NULL, &mtx, 0, 0);
+	device.getContext()->UpdateSubresource(constantbuffer, 0, NULL, &mtx, 0, 0); // CPU によって、マッピング不可能なメモリー内に作成されたサブリソースにメモリーからデータがコピーされる
 	// Bufferをパイプラインにセット
-	device.getContext()->VSSetConstantBuffers(0, 1, &constantbuffer);
+	device.getContext()->VSSetConstantBuffers(0, 1, &constantbuffer); // 頂点シェーダーのパイプライン ステージで使用される定数バッファーを設定
 
 	// テクスチャの読み込み
 	Texture2D tex,tex2;
@@ -139,7 +138,7 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		tex.getSRV(),
 		tex2.getSRV()
 	};
-	device.getContext()->PSSetShaderResources(0, 2, srv);
+	device.getContext()->PSSetShaderResources(0, 2, srv); // ピクセル シェーダー ステージにシェーダー リソースの配列をバインド
 
 
 	int ret = 0;
@@ -155,7 +154,7 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		device.getSwapChain()->Present(0, 0);
 	}
 
-
+	// マクロリリース
 	SAFE_RELEASE(vertexbuffer);
 	SAFE_RELEASE(vs_buf);
 	SAFE_RELEASE(ps_buf);
